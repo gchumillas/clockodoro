40201ad77b37fc984710694a3f08ea6deb3cad15
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-22 18:48:19 +0200

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
