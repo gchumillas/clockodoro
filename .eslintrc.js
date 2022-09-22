0737da665c1ab53eaff5b2ca944e3dea23a203ea
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-22 18:54:01 +0200

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+  settings: {
+    react: {
+      version: 'detect'
     }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
 }
