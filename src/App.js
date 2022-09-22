6fc02684d42eabe4909ccf918332a6094d46fadc
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-22 19:55:06 +0200

    eslint fixes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { useFonts } from 'expo-font'
-import { ShareTechMono_400Regular as shareTechMono400Regular } from '@expo-google-fonts/share-tech-mono'
+import {
+  ShareTechMono_400Regular as shareTechMono400Regular
+} from '@expo-google-fonts/share-tech-mono'
 
 const Loading = _ => {
   // we can't use tailwind features since it's not yet ready
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems:
+        'center',
+        justifyContent: 'center',
+        backgroundColor: 'black'
+      }}
+    >
       <ActivityIndicator size="large" color="white" />
       <StatusBar style="auto" />
     </View>
