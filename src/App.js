8e063e8c588911b723dc2b340607bc4b5976026e
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-27 19:30:17 +0200

    setp react-router-native

--- src/App.js
+++ src/App.js
@@ -1,72 +1,15 @@
 import React from 'react'
-import { StyleSheet, View, ActivityIndicator } from 'react-native'
-import { useKeepAwake } from 'expo-keep-awake'
-import { StatusBar } from 'expo-status-bar'
-import { useFonts } from 'expo-font'
-import {
-  ShareTechMono_400Regular as shareTechMono400Regular
-} from '@expo-google-fonts/share-tech-mono'
-import { palette } from '~/src/constants'
-import { useBatteryLevel } from '~/src/libs/battery'
-import dayjs from '~/src/libs/dayjs'
-import Text from '~/src/components/Text'
-import BatteryIcon from '~/src/components/BatteryIcon'
+import { NativeRouter, Routes, Route } from 'react-router-native'
+import HomePage from '~/src/pages/HomePage'
 
-const Loading = _ => (
-  <View
-    style={{
-      flex: 1,
-      alignItems:
-        'center',
-      justifyContent: 'center',
-      backgroundColor: 'black'
-    }}
-  >
-    <ActivityIndicator size="large" color="white" />
-    <StatusBar style="light" />
-  </View>
-)
-
-export default function App () {
-  useKeepAwake()
-  const level = useBatteryLevel()
-  const [fontsLoaded] = useFonts({ shareTechMono400Regular })
-  const [time, setTime] = React.useState('')
-  const [date, setDate] = React.useState('')
-
-  React.useEffect(() => {
-    const i = setInterval(() => {
-      const now = dayjs()
-      setTime(now.format('HH:mm'))
-      setDate(now.format('ll'))
-    }, 333)
-
-    return () => clearInterval(i)
-  }, [])
-
-  if (!fontsLoaded) {
-    return <Loading />
-  }
-
+const App = () => {
   return (
-    <View style={styles.root}>
-      <Text fontSize={60}>
-        {time}
-      </Text>
-      <Text>
-        {date}
-      </Text>
-      <BatteryIcon value={100 * level} />
-      <StatusBar hidden />
-    </View>
+    <NativeRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </NativeRouter>
   )
 }
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: palette.black,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+export default App
---
+++ src/pages/HomePage.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { StyleSheet, View, ActivityIndicator } from 'react-native'
+import { useKeepAwake } from 'expo-keep-awake'
+import { StatusBar } from 'expo-status-bar'
+import { useFonts } from 'expo-font'
+import {
+  ShareTechMono_400Regular as shareTechMono400Regular
+} from '@expo-google-fonts/share-tech-mono'
+import { palette } from '~/src/constants'
+import { useBatteryLevel } from '~/src/libs/battery'
+import dayjs from '~/src/libs/dayjs'
+import Text from '~/src/components/Text'
+import BatteryIcon from '~/src/components/BatteryIcon'
+
+const Loading = _ => (
+  <View
+    style={{
+      flex: 1,
+      alignItems:
+        'center',
+      justifyContent: 'center',
+      backgroundColor: 'black'
+    }}
+  >
+    <ActivityIndicator size="large" color="white" />
+    <StatusBar style="light" />
+  </View>
+)
+
+const HomePage = () => {
+  useKeepAwake()
+  const level = useBatteryLevel()
+  const [fontsLoaded] = useFonts({ shareTechMono400Regular })
+  const [time, setTime] = React.useState('')
+  const [date, setDate] = React.useState('')
+
+  React.useEffect(() => {
+    const i = setInterval(() => {
+      const now = dayjs()
+      setTime(now.format('HH:mm'))
+      setDate(now.format('ll'))
+    }, 333)
+
+    return () => clearInterval(i)
+  }, [])
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <View style={styles.root}>
+      <Text fontSize={60}>
+        {time}
+      </Text>
+      <Text>
+        {date}
+      </Text>
+      <BatteryIcon value={100 * level} />
+      <StatusBar hidden />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: palette.black,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default HomePage
