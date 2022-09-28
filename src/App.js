28e3cccc040d17a18959e545caedde066a736746
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-28 23:18:07 +0200

    setup react-redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NativeRouter, Routes, Route } from 'react-router-native'
+import { Provider } from 'react-redux'
 import { useFonts } from 'expo-font'
 import {
   ShareTechMono_400Regular as shareTechMono400Regular
@@ -8,6 +9,7 @@ import Loading from '~/src/components/Loading'
 import HomePage from '~/src/pages/HomePage'
 import SettingsPage from '~/src/pages/SettingsPage'
 import '~/src/intl'
+import store from '~/src/store'
 
 const App = () => {
   const [fontsLoaded] = useFonts({ shareTechMono400Regular })
@@ -17,12 +19,14 @@ const App = () => {
   }
 
   return (
-    <NativeRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
-    </NativeRouter>
+    <Provider store={store}>
+      <NativeRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </NativeRouter>
+    </Provider>
   )
 }
 
