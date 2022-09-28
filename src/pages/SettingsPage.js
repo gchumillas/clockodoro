72cd47214af9e0e1e845d5e07cea3f824cdd8f13
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-28 18:35:27 +0200

    use CheckboxField comps

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,13 +1,43 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
-import Text from '~/src/components/Text'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import CheckboxField from '~/src/components/CheckboxField'
 
 const SettingsPage = () => {
+  const [showSeconds, setShowSeconds] = React.useState(false)
+  const [showDate, setShowDate] = React.useState(true)
+  const [showBattery, setShowBattery] = React.useState(true)
+
   return (
-    <SafeAreaView>
-      <Text>settings page</Text>
+    <SafeAreaView style={styles.root}>
+      <CheckboxField
+        label="Show seconds"
+        value={showSeconds} onChange={setShowSeconds}
+        style={styles.field}
+      />
+      <CheckboxField
+        label="Show date"
+        value={showDate} onChange={setShowDate}
+        style={styles.field}
+      />
+      <CheckboxField
+        label="Show battery"
+        value={showBattery} onChange={setShowBattery}
+        style={styles.field}
+      />
+      <StatusBar style="light" />
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    margin: 16
+  },
+  field: {
+    marginBottom: 8
+  }
+})
 
 export default SettingsPage
