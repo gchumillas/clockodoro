aabc5b681ea085d3fa386fc242f686e176fe7fac
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-29 07:13:07 +0200

    eslint: update max-len rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
   ],
   'ignorePatterns': ['*.config.js'],
   'rules': {
-    'max-len': ['warn', { 'code': 80 }],
+    'max-len': ['error', { 'code': 80, 'ignorePattern': 'TODO:' }],
     'eqeqeq': 'off',
     'semi': ['error', 'never'],
     'quotes': ['error', 'single'],
