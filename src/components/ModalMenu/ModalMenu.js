c6eed00386dd2883b8a74a05382dd194bc814b53
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-10-03 09:31:56 +0200

    declare ModalMenu comp

---
+++ src/components/ModalMenu/ModalMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import Text from '~/src/components/Text'
+
+const ModalMenu = () => {
+  return (
+    <View>
+      <Text>modal menu</Text>
+    </View>
+  )
+}
+
+export default ModalMenu
